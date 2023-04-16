8a65825999817534246a5c46d9c14712de2d117b
Prashant Kalsariya <chhabilbhaikalsariya@gmail.com>
2023-04-16 22:29:44 +0530

    new commit

--- PHP/process.php
+++ PHP/process.php
@@ -8,35 +8,43 @@ session_start();
 if (isset($_POST['submit'])) {
     include 'C:\xampp\htdocs\Project_practies\PHP\connection.php';
     $price = mysqli_real_escape_string($con, $_POST['price']);
-    $insert_query = "insert into practice (price) values ('$price')";
-    $price = mysqli_real_escape_string($con, $_POST['price']);
     $name = mysqli_real_escape_string($con, $_POST['name']);
     $disscount = mysqli_real_escape_string($con, $_POST['disscount']);
     $getby = mysqli_real_escape_string($con, $_POST['getby']);
-    $insert_query = "insert into practice2 (price,name,disscount,getby) values ('$price','$name','$disscount','$getby')";
-    $query = mysqli_query($con, $insert_query);
-    if ($query) {
-        $select_query = "select * from practice2 where name='$name'";
-
-        $query2 = mysqli_query($con, $select_query);
-        $arrdata = mysqli_fetch_array($query2);
-        $_SESSION['price'] = $arrdata['price'];
-        $_SESSION['id'] = $arrdata['id'];
+    $select_query = "select * from practice2 where name = '$name'";
+    $squery = mysqli_query($con, $select_query);
+    if (mysqli_num_rows($squery) > 0) {
 ?>
         <script>
-            alert('Add item to cart succesfully!! Your id is <?php echo $arrdata['id']; ?> Please not down it for future uses');
+            alert('It is already exist in Cart');
             location.replace('/Project_practies/HTML/index.html');
         </script>
-    <?php
-        // header('location : /Project_practies/HTML/index.html');
+        <?php
     } else {
-    ?>
-        <script>
-            alert('can not add item into cart');
-        </script>
-<?php
-    }
-}
-// }
+        $insert_query = "insert into practice2 (price,name,disscount,getby) values ('$price','$name','$disscount','$getby')";
+        $query = mysqli_query($con, $insert_query);
+        if ($query) {
+            $select_query = "select * from practice2 where name='$name'";
 
+            $query2 = mysqli_query($con, $select_query);
+            $arrdata = mysqli_fetch_array($query2);
+            $_SESSION['price'] = $arrdata['price'];
+            $_SESSION['id'] = $arrdata['id'];
+        ?>
+            <script>
+                alert('Add item to cart succesfully!! Your id is <?php echo $arrdata['id']; ?> Please not down it for future uses');
+                location.replace('/Project_practies/HTML/index.html');
+            </script>
+        <?php
+            // header('location : /Project_practies/HTML/index.html');
+        } else {
+        ?>
+            <script>
+                alert('can not add item into cart');
+            </script>
+<?php
+        }
+    }
+    // }
+}
 ?>
